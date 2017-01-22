ee0c53ec249c987e788fcf3c37a777600dc2cfa2
rexbut <butbutdu45@gmail.com>
2017-01-22 20:30:10 +0100

    Update : Command

--- src/main/java/fr/evercraft/everkits/command/sub/EKReload.java
+++ src/main/java/fr/evercraft/everkits/command/sub/EKReload.java
@@ -17,6 +17,7 @@
 package fr.evercraft.everkits.command.sub;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 import org.spongepowered.api.command.CommandException;
@@ -45,7 +46,7 @@ public class EKReload extends ESubCommand<EverKits> {
 		return EAMessages.RELOAD_DESCRIPTION.getText();
 	}
 	
-	public List<String> subTabCompleter(final CommandSource source, final List<String> args) throws CommandException {
+	public Collection<String> subTabCompleter(final CommandSource source, final List<String> args) throws CommandException {
 		return new ArrayList<String>();
 	}
 
